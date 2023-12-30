207162643c3667702cc99e1ece3d2e4d6543737c
JYRRC <javixhtsweet@gmail.com>
2023-12-30 16:35:06 +0100

    marble: drop viperFX
    Signed-off-by: JYRRC <javixhtsweet@gmail.com>

--- device.mk
+++ device.mk
@@ -630,8 +630,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
-# Viper
-$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
 # VENDOR API
 BOARD_API_LEVEL := 31
